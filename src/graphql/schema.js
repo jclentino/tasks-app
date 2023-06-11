bf6e8db76b776086b8a816ba56c486857f038761
jclentino <nuncaparesdecrear@gmail.com>
2023-06-10 23:05:53 -0500

    Schema: Creando raíz con querys y mutaciones

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,14 +1,25 @@
-const { welcomeMessage } = require('./querys')
 const { GraphQLSchema, GraphQLObjectType } = require('graphql')
+const { user, users } = require('./querys')
+const { register } = require('./mutations')
 
 const QueryType = new GraphQLObjectType({
     name: "QueryType",
     description: "The root query type",
     fields: {
-        welcomeMessage
+        user,
+        users,
+    }
+})
+
+const MutationType = new GraphQLObjectType({
+    name: "MutationType",
+    description: "The root mutation type",
+    fields: {
+        register,
     }
 })
 
 module.exports = new GraphQLSchema({
     query: QueryType,
+    mutation: MutationType
 })
