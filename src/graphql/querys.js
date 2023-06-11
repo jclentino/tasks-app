cdc7dc0af8bce2c8673072392c5076f74d939ac4
jclentino <nuncaparesdecrear@gmail.com>
2023-06-10 23:02:05 -0500

    Querys: Creando query para obtener un usuario por su identificador

--- src/graphql/querys.js
+++ src/graphql/querys.js
@@ -1,8 +1,23 @@
-const { GraphQLList } = require('graphql')
+const { GraphQLID, GraphQLList } = require('graphql')
 const { UserType } = require('./typedef')
 const { User } = require('../models')
 
 // Users 
+const user = {
+    type: UserType, 
+    description: 'Get a User by Id',
+    args: {
+        id: { type: GraphQLID },
+    },
+    resolve: (__, { id })=> {
+        try {
+            return User.findById(id)
+        } catch (e){
+            throw new Error(e)
+        }
+    }
+}
+
 const users = {
     type: new GraphQLList(UserType), 
     description: 'List of Users',
@@ -15,4 +30,4 @@ const users = {
     }
 }
 
-module.exports = { users }
+module.exports = { user, users }
