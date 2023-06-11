910af6472dede5aa5d3cf86cb114635c4de6c2fa
jclentino <nuncaparesdecrear@gmail.com>
2023-06-10 22:52:30 -0500

    Typedef: Creando definición de tipos de User

---
+++ src/graphql/typedef.js
@@ -0,0 +1,16 @@
+const { GraphQLObjectType, GraphQLID, GraphQLString,  } = require('graphql')
+
+const UserType = new GraphQLObjectType({
+    name: 'UserType',
+    description: 'The user type',
+    fields: {
+        id: { type: GraphQLID },
+        username: { type: GraphQLString },
+        email: { type: GraphQLString },
+        displayName: { type: GraphQLString },
+        createdAt: { type: GraphQLString},
+        updatedAt: { type: GraphQLString }
+    }
+})
+
+module.exports = { UserType }
